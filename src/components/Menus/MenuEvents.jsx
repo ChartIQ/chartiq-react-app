d6b98994f9dbfb17a1353f5b698fbff49510eedd
mwilber-ciq <48366492+mwilber-ciq@users.noreply.github.com>
2019-04-30 16:30:49 -0400

    13298 event menu tap event (#12)
    * Add custom event handler for stxtap
    
    * Add refs and event handlers for all menu items
    
    * Get rid of all the refs
    - Add one ref for the dropdown group
    - Use queryselector to attach event listeners to cq-item nodes
    
    * Remove jquery calls
    
    * strip jQuery from helicopter function
    
    * fix helicopter propeller
    
    * remove unused import

--- src/components/Menus/MenuEvents.jsx
+++ src/components/Menus/MenuEvents.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Lookup, Menu, MenuDropDown, Scroll } from 'components'
 import { ChartContext } from '../../react-chart-context'
 
 /**
@@ -14,19 +13,52 @@ import { ChartContext } from '../../react-chart-context'
  * @extends {React.Component}
  */
 export default class MenuEvents extends React.Component {
+
+	constructor(){
+		super();
+		this.markerDropdown = React.createRef();
+	}
+
+	componentDidMount(){
+		// Attach an stxtap event handler to all cq-item tags in the events menu
+		[...this.markerDropdown.current.querySelectorAll('cq-item')].forEach((menuitem) => {
+			// Skip menu items that don't have a markertype attribute
+			if(menuitem.attributes.markertype){
+				menuitem.addEventListener('stxtap', this.eventStxtapHandler.bind(this));
+			}
+		});
+	}
+
+	eventStxtapHandler(e){
+		if(e.target.attributes.markertype){
+			this.activateMarker(e.target, e.target.attributes.markertype.value);
+		}else{
+			console.log('Event marker type not found.')
+		}
+	}
+
+	resetMarkerMenu(){
+		// Remove active class from all menu item radio buttons
+		[...this.markerDropdown.current.querySelectorAll('cq-item .ciq-radio')].forEach((menuitem) => {
+			menuitem.classList.remove("ciq-active");
+		});
+	}
+
 	activateMarker(target, type) {
-		const chart = $$$('chartiq-chart')
-		$$$(".stx-markers .ciq-active").classList.remove("ciq-active")
-		target.classList.add("ciq-active")
-		if (type == "helicopter") this.activateHelicopter()
-		else if (type != "none") chart.showMarkers(type)
-		else chart.hideMarkers()
+		let chart = this.context.stx.container;
+		this.resetMarkerMenu();
+		target.querySelector('.ciq-radio').classList.add("ciq-active");
+		if (type == "helicopter") 
+			this.activateHelicopter();
+		else if (type == "none") 
+			chart.hideMarkers();
+		else 
+			chart.showMarkers(type);
 	}
 
 	activateHelicopter() {
-		let helicopter = CIQ.UI.makeFromTemplate($$$("template.abstract"))[0]
-		let chart = $$$('chartiq-chart')
-		chart.hideMarkers()
+		let helicopter = CIQ.UI.makeFromTemplate(document.querySelector("template.abstract"))[0];
+		this.context.stx.container.hideMarkers();
 		
 		var marker=new CIQ.Marker({
 			stx: this.context.stx,
@@ -37,25 +69,27 @@ export default class MenuEvents extends React.Component {
 			chartContainer: true,
 			node: helicopter
 		});
-		var leftPositition = this.context.stx.chart.width*0.4
-		$$$('div.stx-marker.abstract').setAttribute('style', `z-index: 21; left: ${leftPositition}px`)
-		this.context.stx.draw()
+
+		var leftPositition = this.context.stx.chart.width*0.4;
+		document.querySelector('div.stx-marker.abstract').setAttribute('style', `z-index: 21; left: ${leftPositition}px`);
+		
+		this.context.stx.draw();
 	}
 
 	render () {
 		return (
 			<cq-menu class="ciq-menu stx-markers collapse">
 				<span>Events</span>
-				<cq-menu-dropdown>
-					<cq-item className="square" onClick={(e) => this.activateMarker(e.target, 'square')}>Simple Square<span className="ciq-radio"><span></span></span>
+				<cq-menu-dropdown ref={this.markerDropdown}>
+					<cq-item className="square" markertype="square">Simple Square<span className="ciq-radio"><span></span></span>
 					</cq-item>
-					<cq-item className="circle" onClick={(e) => this.activateMarker(e.target, 'circle')}>Simple Circle<span className="ciq-radio"><span></span></span>
+					<cq-item className="circle" markertype="circle">Simple Circle<span className="ciq-radio"><span></span></span>
 					</cq-item>
-					<cq-item className="callout" onClick={(e) => this.activateMarker(e.target, 'callout')}>Callouts<span className="ciq-radio"><span></span></span>
+					<cq-item className="callout" markertype="callout">Callouts<span className="ciq-radio"><span></span></span>
 					</cq-item>
-					<cq-item className="abstract" onClick={(e) => this.activateMarker(e.target, 'helicopter')}>Abstract<span className="ciq-radio"><span></span></span>
+					<cq-item className="abstract" markertype="helicopter">Abstract<span className="ciq-radio"><span></span></span>
 					</cq-item>
-					<cq-item className="none"onClick={(e) => this.activateMarker(e.target, 'none')}>None<span className="ciq-radio ciq-active"><span></span></span>
+					<cq-item className="none" markertype="none">None<span className="ciq-radio ciq-active"><span></span></span>
 					</cq-item>
 				</cq-menu-dropdown>
 			</cq-menu>
