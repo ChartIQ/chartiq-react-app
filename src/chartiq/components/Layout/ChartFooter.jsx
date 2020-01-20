e2c2501f1beee585ebae47ebbf6d47996147a24e
normunds-ciq <53879914+normunds-ciq@users.noreply.github.com>
2020-01-20 17:09:49 -0500

    Disable share button when functionality is not available in license (#40)
    * Disable share button when functionality is not available in license
    
    By default configuration property footerShare is set to true. To provide better developer experience warning in console is issued when functionality requested in property is not available informing developer using library why the share button is not available .
    
    * Correct study component error

--- src/chartiq/components/Layout/ChartFooter.jsx
+++ src/chartiq/components/Layout/ChartFooter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { CIQ } from 'chartiq/js/chartiq';
 import { ChartContext } from '../../context/ChartContext';
 import { ShareChart, RangeBar } from '../Features';
 
@@ -14,11 +14,21 @@ import { ShareChart, RangeBar } from '../Features';
  */
 export default class ChartFooter extends React.Component {
 	
+	componentDidMount() {
+		const { footerShare } = this.context.config;
+		if (footerShare && !CIQ.Share) {
+			console.warn(
+				'Share is set true in configuration but share is not available in license. ' +
+				'Set config.footerShare to false to disable this warning' )
+		}
+	}
+	
 	render() {
 		const { footerShare } = this.context.config;
+		const shareAvailable = !!CIQ.Share;
 		const footer = (
 			<div className="ciq-footer full-screen-hide">
-				{footerShare && <ShareChart />}
+				{footerShare && shareAvailable && <ShareChart />}
 				<RangeBar />
 			</div>
 		);
