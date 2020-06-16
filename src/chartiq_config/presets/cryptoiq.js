c0b4eb2b941dc0e5ff15ebfc30e4e7f86a8a9fd7
Jon Eckenrode <jeckenrode@chartiq.com>
2020-06-16 19:01:47 -0400

    Changed the Active Trader plug-in path to chartiq/plugins/activetrader/.

--- src/chartiq_config/presets/cryptoiq.js
+++ src/chartiq_config/presets/cryptoiq.js
@@ -1,7 +1,6 @@
 export default () => Promise.all([
-  import(/* webpackChunkName: "cryptoiq" */ 'chartiq/plugins/cryptoiq/cryptoiq'),
-  import(/* webpackChunkName: "marketdepth" */ 'chartiq/plugins/cryptoiq/marketdepth'),
-  import(/* webpackChunkName: "orderbook" */ 'chartiq/plugins/cryptoiq/orderbook'),
+  import(/* webpackChunkName: "cryptoiq" */ 'chartiq/plugins/activetrader/cryptoiq'),
+  import(/* webpackChunkName: "marketdepth" */ 'chartiq/plugins/activetrader/marketdepth'),
+  import(/* webpackChunkName: "orderbook" */ 'chartiq/plugins/activetrader/orderbook'),
   import(/* webpackChunkName: "L2_simulator" */ 'chartiq/examples/feeds/L2_simulator')
 ]);
-
