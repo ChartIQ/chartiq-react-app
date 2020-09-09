ac3454536bd4a9779b332058ee3177e886c3ea1e
Jon Eckenrode <jeckenrode@chartiq.com>
2020-09-09 10:44:50 -0400

    Revised the component names on the route list home page.

--- src/chartiq/containers/RouteList/RouteList.jsx
+++ src/chartiq/containers/RouteList/RouteList.jsx
@@ -30,20 +30,20 @@ export default class RouteList extends React.Component {
 
         <ul className="top-level">
           <li>
-            <h3><Link to="/technical-analysis">AdvancedChartComponent</Link></h3>
+            <h3><Link to="/technical-analysis">AdvancedChart</Link></h3>
             <p>
               Creates a chart with a full-featured user interface. AdvancedChartComponent is
               the equivalent of ChartIQ's technical-analysis-chart.html advanced template.
             </p>
           </li>
           <li>
-          <h3><Link to="/multi-chart">MultiChartComponent</Link></h3>
+          <h3><Link to="/multi-chart">MultiChart</Link></h3>
             <p>
               Displays two advanced chart components side by side in the same document.
             </p>
           </li>
           <li>
-            <h3><Link to="/active-trader">ActiveTraderComponent</Link></h3>
+            <h3><Link to="/active-trader">ActiveTraderWorkstation</Link></h3>
             <p>
               Features the advanced chart component enhanced with the following plug-ins:
             </p>
@@ -55,13 +55,13 @@ export default class RouteList extends React.Component {
               </ul>
           </li>
           <li>
-            <h3><Link to="/custom-chart">CustomChartComponent</Link></h3>
+            <h3><Link to="/custom-chart">CustomChart</Link></h3>
             <p>
               Integrates native React components with ChartIQ web components.
             </p>
           </li>
           <li>
-            <h3><Link to="/hello-world">HelloWorldComponent</Link></h3>
+            <h3><Link to="/hello-world">HelloWorld</Link></h3>
             <p>
               Creates a basic chart with no user interface as a starting point for using the ChartIQ
               API in React.
