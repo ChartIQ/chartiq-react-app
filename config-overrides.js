13f3b9aeb7b6ba938fa5b3940af108252a7ff2ed
normunds-ciq <normunds@chartiq.com>
2020-10-23 16:26:30 -0400

    Remove config-override.js
    This file is no longer required in v8.1

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override(config, env) {
-  config.module.rules.push({ // add html loader required for tfc, scriptiq, cryptoiq
-    test: /\.html/,
-    exclude: /\.css/,
-    use: [{ loader: 'html-loader' }]
-  });
-
-  return config;
-} 
