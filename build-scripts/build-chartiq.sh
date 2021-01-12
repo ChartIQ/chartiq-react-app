641cbbb66e257854583b6a15c7a4403026a132e7
nikcodeit <mykyta.lastovych@codeit.pro>
2021-01-12 17:36:02 +0200

    Building workflow

--- build-scripts/build-chartiq.sh
+++ build-scripts/build-chartiq.sh
@@ -2,12 +2,12 @@
 # Then copies and installs the package into the react app.
 
 echo "Create library tarball file" && \
-cd ../stx && \
+cd ./stx && \
 npm run packtgz && \
 
 echo "Copy tarball files from stx" && \
-cd ../chartiq-react-app-private && \
-cp ../stx/dist/chartiq-*.*.*.tgz ./ && \
+cd ../ && \
+cp ./stx/dist/chartiq-*.*.*.tgz ./ && \
 
 echo "Install library" && \
 npm uninstall chartiq && \
--- build-scripts/build-test.sh
+++ build-scripts/build-test.sh
@@ -2,12 +2,12 @@
 # Then copies and installs the package into the react app.
 
 echo "Create testing tarball file" && \
-cd ../stx/spec && \
+cd ./stx/spec && \
 npm run prepare && npm pack && \
 
 echo "Copy tarball files from stx" && \
-cd ../../chartiq-react-app-private && \
-cp ../stx/spec/chartiq-ui-tests-*.*.*.tgz ./ && \
+cd ../ && \
+cp ./stx/spec/chartiq-ui-tests-*.*.*.tgz ./ && \
 
 echo "Install tests" && \
 npm uninstall selenium-standalone @chartiq/ui-tests && \
