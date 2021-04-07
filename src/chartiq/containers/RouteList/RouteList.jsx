a027e1864577607f127fc5d6ae45450dff880b55
Jon Eckenrode <jeckenrode@chartiq.com>
2021-04-07 17:32:49 -0400

    Revised Term Structure link.

--- src/chartiq/containers/RouteList/RouteList.jsx
+++ src/chartiq/containers/RouteList/RouteList.jsx
@@ -64,15 +64,18 @@ export default class RouteList extends React.Component {
               </ul>
           </li>
           <li>
-            <h3 className="disabled-link" style={{ color: '#888' }}>TermStructure Chart
+            <h3 className="disabled-link" style={{ color: '#888', marginBottom: 0 }}>TermStructure</h3>
             <p style={{fontSize: 0.75 + "rem", marginTop: 0, marginBottom: 0.70 + "rem"}}>
               (To enable this link, uncomment all lines in the <i>src</i> directory following the <code>// Enable TermStructure</code> comment.)
             </p>
-            </h3>
-            {/* Enable TermStructure */}
-            {/* <h3><Link to="term-structure">Term Structure Chart</Link></h3> */}
+            {
+              /*
+              Enable TermStructure
+              <h3><Link to="term-structure">Term Structure Chart</Link></h3>
+              */
+            }
             <p>
-              Creates a Term Structure chart that can be used for non time series data
+              Creates a term structure chart for working with non&ndash;time series data.
             </p>
           </li>
           <li>
