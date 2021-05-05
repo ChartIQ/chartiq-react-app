7bdfb9c5fa8c771c7a5e50c5d7d874eb24d02c35
nikcodeit <mykyta.lastovych@codeit.pro>
2021-05-05 13:02:32 +0300

    Fixed spec paths in wdio-ff config

--- wdio-ff.conf.js
+++ wdio-ff.conf.js
@@ -4,6 +4,7 @@ const { config } = require('./stx/spec/e2e-new/wdio-ff.conf');
 const AllureReporter = require('@wdio/allure-reporter').default;
 const path = require('path');
 const fs = require('fs');
+const StaticServer = require('static-server');
 global.downloadDir = path.join(__dirname, 'stx/spec/e2e-new/tempDownload');
 
 const browsers = [
@@ -32,32 +33,33 @@ const wdioConfig = {
 		appName: 'chartiq-react-app',
 		specs: ci ? [
 			[
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/periodicity-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/display-chartstyles-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/table-view.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-advanced.spec.js"
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/periodicity-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/display-chartstyles-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/table-view.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-advanced.spec.js"
 			],
 			[
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-beta-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/pref-locale-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/view-advanced.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/headsup-adv.spec.js"
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-beta-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/pref-locale-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/view-advanced.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/headsup-adv.spec.js"
 			],
 			[
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-perf-index-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/markers-menu-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/comparison-component-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/drawing-pallete-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/menu-dropdown-component-adv.spec.js"
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-perf-index-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/markers-menu-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/comparison-component-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/drawing-pallete-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/menu-dropdown-component-adv.spec.js"
 			],
 			[
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-price-relative-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/symbol-search-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/full-screen-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/message-toaster-adv.spec.js",
-				".stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-dialog-adv.spec.js"
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-price-relative-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/symbol-search-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/full-screen-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/message-toaster-adv.spec.js",
+				"./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/studies-dialog-adv.spec.js"
 			]
-		] : ['./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/**.spec.js'],		// Because we have different names for our templates across projects, we are accessing them thru configured variables.
+		] : ['./stx/spec/e2e-new/test/specs/e2e/sample-template-advanced/**.spec.js'],
+		// Because we have different names for our templates across projects, we are accessing them thru configured variables.
 		// The template object is a way to set the name of the component's file for this specific repo.
 		// NOTE: this is something custom to our configs not a default option of WDIO
 		baseUrl: 'http://localhost:4040',
@@ -77,7 +79,6 @@ const wdioConfig = {
 				fs.rmdirSync(global.downloadDir, { recursive: true });
 				fs.mkdirSync(global.downloadDir);
 			}
-			var StaticServer = require('static-server');
 			var server = new StaticServer({
 				rootPath: './dist/',            // required, the root of the server file tree
 				port: 4040,               // required, the port to listen
