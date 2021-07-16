b9c63bf0723d1a1ba71555196bc37e8d1416d962
nikcodeit <mykyta.lastovych@codeit.pro>
2021-07-16 05:41:06 -0400

    Removed duplicated code

--- wdio-chrome.conf.js
+++ wdio-chrome.conf.js
@@ -1,23 +1,5 @@
-let debug = process.env.DEBUG;
 const { config } = require('./stx/tests/e2e-v2/wdio-chrome.conf');
 const { wdioConfigReact } = require('./wdio.conf.js')
-
-const browsers = [
-	{
-		maxInstances: debug ? 1 : 5,
-		browserName: 'chrome',
-		'goog:chromeOptions': {
-			args: debug
-				? ['--incognito', '--no-sandbox', '--test-type=browser', '--start-maximized', '--window-size=800,600']
-				: ['--incognito', '--no-sandbox', '--test-type=browser', '--start-maximized', '--window-size=800,600', '--headless'],
-			prefs: {
-				directory_upgrade: true,
-				prompt_for_download: false,
-				'download.default_directory': global.downloadDir,
-			},
-		},
-	},
-];
 
 const wdioConfig = {
 	...config,
--- wdio-ff.conf.js
+++ wdio-ff.conf.js
@@ -1,23 +1,5 @@
-let debug = process.env.DEBUG;
 const { config } = require('./stx/tests/e2e-v2/wdio-ff.conf');
 const { wdioConfigReact } = require('./wdio.conf.js')
-
-const browsers = [
-	{
-		maxInstances: debug ? 1 : 5,
-		browserName: 'firefox',
-		'moz:firefoxOptions': {
-			args: debug ? [] : ['-headless'],
-			prefs: {
-				directory_upgrade: true,
-				prompt_for_download: false,
-				"browser.helperApps.neverAsk.saveToDisk": "text/csv",
-				"browser.download.folderList": 2,
-				"browser.download.dir": global.downloadDir
-			},
-		},
-	},
-];
 
 const wdioConfig = {
 	...config,
