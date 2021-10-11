1a6f435eefa6f366344300a909b3413959efce9e
nikcodeit <mykyta.lastovych@codeit.pro>
2021-10-11 09:24:30 -0400

    commented tests groups

--- wdio.conf.js
+++ wdio.conf.js
@@ -16,28 +16,28 @@ exports.wdioConfigReact = {
 			"./stx/tests/e2e-v2/specs/sample-template-advanced/full-screen-adv.spec.js",
 			"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-dialog-adv.spec.js",
 		],
-		[
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-price-relative-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/pref-locale-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/display-chartstyles-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/drawing-palette-adv.spec.js",
-		],
-		[
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/table-view.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/dialogs/data-dialog.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-beta-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/comparison-component-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/message-toaster-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/menu-dropdown-component-adv.spec.js"
-		],
-		[
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/periodicity-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/symbol-search-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/markers-menu-adv.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-advanced.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/span-options.spec.js",
-			"./stx/tests/e2e-v2/specs/sample-template-advanced/dialogs/aggregation-dialog-adv.spec.js"
-		]
+		// [
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-price-relative-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/pref-locale-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/display-chartstyles-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/drawing-palette-adv.spec.js",
+		// ],
+		// [
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/table-view.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/dialogs/data-dialog.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-beta-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/comparison-component-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/message-toaster-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/menu-dropdown-component-adv.spec.js"
+		// ],
+		// [
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/periodicity-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/symbol-search-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/markers-menu-adv.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/studies-advanced.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/span-options.spec.js",
+		// 	"./stx/tests/e2e-v2/specs/sample-template-advanced/dialogs/aggregation-dialog-adv.spec.js"
+		// ]
 	] : ['./stx/tests/e2e-v2/specs/sample-template-advanced/**/**.spec.js'],
 	// Because we have different names for our templates across projects, we are accessing them thru configured variables.
 	// The template object is a way to set the name of the component's file for this specific repo.
