ebd409d408d7e939feb6e953e25e849e3ed341d7
Ryan McNeely <ryan.mcneely@pm.me>
2021-11-23 10:27:05 -0500

    Add AdvancedChartPage import back into main

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ import {
 	HelloWorld
 } from "@chartiq/chartiq-react-components";
 
+import AdvancedChartPage from "./containers/AdvancedChart/AdvancedChartPage";
+
 // Enable the following import if the channelSubscribe reference or the call to CIQ.simulateL2 in the chartInitialized reference are required.
 // import { CIQ } from 'chartiq/js/componentUI';
 // const { channelSubscribe } = CIQ.UI.BaseComponent.prototype;
