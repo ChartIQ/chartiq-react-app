285da11ad82ad88b63f2c8a09df6db97dced7001
Ryan McNeely <ryan.mcneely@pm.me>
2021-12-07 15:50:23 -0500

    Fixes for CSS Styling when TermStructure is imported

--- src/chartiq-react-components/addCSSfiles.sh
+++ src/chartiq-react-components/addCSSfiles.sh
@@ -2,3 +2,4 @@
 
 cp containers/AdvancedChart/*.css dist/containers/AdvancedChart/
 cp containers/ActiveTraderWorkstation/*.css dist/containers/ActiveTraderWorkstation/
+cp styles/library-overrides.css dist/styles/
--- src/chartiq-react-components/styles/base-imports.js
+++ src/chartiq-react-components/styles/base-imports.js
@@ -2,3 +2,4 @@ import 'chartiq/css/normalize.css';
 import 'chartiq/css/page-defaults.css';
 import 'chartiq/css/stx-chart.css';
 import 'chartiq/css/chartiq.css';
+import './library-overrides.css'
