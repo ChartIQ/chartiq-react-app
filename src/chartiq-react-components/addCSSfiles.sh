e47a2ff32e0c996216600955f6102623a7b179bf
Ryan McNeely <ryan.mcneely@pm.me>
2021-12-07 21:42:41 -0500

    Reorganize component files

--- src/chartiq-react-components/addCSSfiles.sh
+++
@@ -1,5 +0,0 @@
-# mkdir -p dist/containers/
-
-cp containers/AdvancedChart/*.css dist/containers/AdvancedChart/
-cp containers/ActiveTraderWorkstation/*.css dist/containers/ActiveTraderWorkstation/
-cp styles/library-overrides.css dist/styles/
