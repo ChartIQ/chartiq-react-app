3bb95dcc4a16c1b16c6b593b672a7922345e228a
Ryan McNeely <ryan.mcneely@pm.me>
2022-01-24 10:48:27 -0500

    Remove deleted file import, change webpack alias

--- chartiq-react-components/src/Advanced/index.js
+++ chartiq-react-components/src/Advanced/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import { default as AdvancedChart, CIQ } from "./Chart";
 
 import { getConfig, getCustomConfig } from "./resources"; // ChartIQ library resources
-import "./Chart.css";
 import ChartPage from "./ChartPage"
 
 export default function ({ chartInitialized, config, resources }) {
